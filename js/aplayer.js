0c66284b9918b3330094f6fe3aecd35af932d64d
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-03-20 01:46:01 +0000

    Deploy Xiaolong00/xiaolong00.github.io to github.com/Xiaolong00/xiaolong00.github.io.git:gh-pages

--- js/aplayer.js
+++ js/aplayer.js
@@ -1,24 +1,25 @@
 !(function() {
-    var oldLoadAp = window.onload;
-    window.onload = function () {
-        oldLoadAp && oldLoadAp();
-
-        new APlayer({
-            container: document.getElementById('aplayer'),
-            fixed: true,
-            autoplay: false,
-            loop: 'all',
-            order: 'random',
-            theme: '#b7daff',
-            preload: 'none',
-            audio: [
-                {
-                    name: 'song1',
-                    artist: 'artist1',
-                    url: '/songs/song1.mp3',
-                    cover: '/img/comm/favicon.jpg'
-                }
-            ]
-        });
-    }
+    document.getElementById("subtitle").addEventListener("click",()=>{window.location.href = "http://106.54.44.194/"});
+    // var oldLoadAp = window.onload;
+    // window.onload = function () {
+    //     oldLoadAp && oldLoadAp();
+    //
+    //     new APlayer({
+    //         container: document.getElementById('aplayer'),
+    //         fixed: true,
+    //         autoplay: false,
+    //         loop: 'all',
+    //         order: 'random',
+    //         theme: '#b7daff',
+    //         preload: 'none',
+    //         audio: [
+    //             {
+    //                 name: 'song1',
+    //                 artist: 'artist1',
+    //                 url: '/songs/song1.mp3',
+    //                 cover: '/img/comm/favicon.jpg'
+    //             }
+    //         ]
+    //     });
+    // }
 })();
